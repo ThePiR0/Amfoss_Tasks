5b6372a9aad7beb0c4ad947fb7d4579843f11a44
Aaryan Ajeeth Kumar <ajeethrilrtk@gmail.com>
2024-12-31 18:39:48 +0530

    Completed

---
+++ Task-03/index.js
@@ -0,0 +1,25 @@
+
+const soundMap = {
+    w: "sounds/snare.mp3",
+    a: "sounds/kick-bass.mp3",
+    s: "sounds/crash.mp3",
+    d: "sounds/tom-1.mp3",  
+    j: "sounds/tom-2.mp3",
+    k: "sounds/tom-3.mp3",
+    l: "sounds/tom-4.mp3",
+  };
+  
+
+  document.querySelectorAll(".drum").forEach(button => {
+    button.addEventListener("click", () => {
+      const soundKey = button.textContent; 
+      playSound(soundKey);
+    });
+  });
+  
+  
+  function playSound(key) {
+    const audio = new Audio(soundMap[key]);
+    audio.play();
+  }
+  
